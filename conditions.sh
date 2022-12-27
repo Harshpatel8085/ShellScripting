43cee5acdedf6061f9e2eaa5eabb6b41841a7781
NIKITA <56646412+Harshpatel8085@users.noreply.github.com>
2022-12-27 11:50:46 -0500

    a

--- conditions.sh
+++ conditions.sh
@@ -14,13 +14,13 @@
 
 c=$1
 
-if ["$c" -eq "10"] ; then
+if [ "$c" -eq "10" ] ; then
     echo "value of c is 10"
 
-elif ["$c" -eq "20"] ; then 
+elif [ "$c" -eq "20" ] ; then 
     echo "value of c is 20"
 
-elif ["$c" -eq "30"] ; then
+elif [ "$c" -eq "30" ] ; then
     echo "value of c is 30"
 
 else
