5850ae8ccd0bc6e55d51bc61e976c6da887b1714
NIKITA <56646412+Harshpatel8085@users.noreply.github.com>
2022-12-27 11:43:48 -0500

    a

---
+++ conditions.sh
@@ -0,0 +1,28 @@
+# if [expression 1]; then
+#     command 1
+#     exit 1 # this exit code is to know which condition is executing, using '$?' command in command line, this is not mandatory.
+# elif [expression 2]; then
+#     command 2
+#     exit 2
+# elif [expression 3]; then
+#     command 3
+#     exit 3
+# else 
+#     command-x
+#     exit 4
+# fi
+
+c=$1
+
+if [$c -eq "10"]; then
+    echo "value of c is 10"
+
+elif [$c -eq "20"]; then 
+    echo "value of c is 20"
+
+elif [$c -eq "30"]; then
+    echo "value of c is 30"
+
+else
+    echo "value is not 10,20 or 30"
+fi
--- functions.sh
+++ functions.sh
@@ -1,6 +1,6 @@
 Harsh (){
     echo -e "my name is\e[34m Harsh\e[0m"
-    echo "Load average on the system in last 1 minute: $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}')"
+    echo "Load average on the system in last 1 minute: $(uptime | awk -F : '{print $NF}' | awk -F  , '{print $1}')"
     echo '$NF' is used to print last field of the row/value, It is called nfh field
 }
 Harsh
