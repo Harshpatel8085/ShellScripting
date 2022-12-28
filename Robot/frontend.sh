e808aeb253813ed018b2822ccbc8b7efffe28a10
NIKITA <56646412+Harshpatel8085@users.noreply.github.com>
2022-12-28 14:57:25 -0500

    a

--- Robot/frontend.sh
+++ Robot/frontend.sh
@@ -3,31 +3,23 @@ if [ $ID -ne 0 ] ; then
     echo -e -n "\e[31m You need to run script as a Root user or with Sudo permission \e[0m" #here -n will delete the line that usually echo leave below the message line
     exit 1
 fi
+stat(){
+    if [ $1 -eq 0 ] ; then
+        echo -e  "\e[32m Success \e[0m"
+    else
+        echo -e  "\e[32m Failure \e[32m"
+    fi
+}
 
 echo "Installing nginx"
 yum install nginx -y
-#stat(){
-if [ $? -eq 0 ] ; then
-    echo -e  "\e[32m Success \e[0m"
-else
-    echo -e  "\e[32m Failure \e[32m"
-fi
-#}
+stat ?
 
 echo "Enabling nginx"
 systemctl enable nginx &>> /tmp/frontend.log
-#stat ?
-if [ $? -eq 0 ] ; then
-    echo -e  "\e[32m Success \e[0m"
-else
-    echo -e  "\e[32m Failure \e[32m"
-fi
+stat ?
 
 echo -n "starting nginx"
 systemctl start nginx &>> /tmp/frontend.log
-#stat ?
-if [ $? -eq 0 ] ; then
-    echo -e  "\e[32m Success \e[0m"
-else
-    echo -e  "\e[32m Failure \e[32m"
-fi
+stat ?
+
