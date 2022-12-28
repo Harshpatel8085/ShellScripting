9dea9e8eec7935bb4407e25f1012cd78894f861c
NIKITA <56646412+Harshpatel8085@users.noreply.github.com>
2022-12-28 13:06:33 -0500

    a

--- Robot/frontend.sh
+++ Robot/frontend.sh
@@ -1,6 +1,6 @@
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then
-    echo -e "\e[31m You need to run script as a Root user or with Sudo permission \e[0m"
+    echo -e -n "\e[31m You need to run script as a Root user or with Sudo permission \e[0m"
     exit 1
 fi
 
