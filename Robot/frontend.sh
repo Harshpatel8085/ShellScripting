afd21a2daa3ec54c1e7bf9b0f1b18868920d24d2
NIKITA <56646412+Harshpatel8085@users.noreply.github.com>
2022-12-28 13:07:58 -0500

    a

--- Robot/frontend.sh
+++ Robot/frontend.sh
@@ -1,6 +1,6 @@
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then
-    echo -e -n "\e[31m You need to run script as a Root user or with Sudo permission \e[0m"
+    echo -e -n "\e[31m You need to run script as a Root user or with Sudo permission \e[0m" #here -n will delete the line that usually echo leave below the message line
     exit 1
 fi
 
